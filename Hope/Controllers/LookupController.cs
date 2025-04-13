c51f94c11fb54585b816af91d4f21adb86c223e0
Abanoub Saleh <abanoub.saleh@asset.com.eg>
2025-04-14 01:42:00 +0200

    create lookup controller

---
+++ Hope/Controllers/LookupController.cs
@@ -0,0 +1,15 @@
+using Hope.Application.Common.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Hope.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LookupController : ControllerBase
+    {
+        // Add your lookup endpoints here
+    }
+}
